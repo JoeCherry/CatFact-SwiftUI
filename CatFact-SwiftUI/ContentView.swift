1fb04824fd6d11061694333581f057ce5ee9aedf
Joe Cherry <josephmcherry13@gmail.com>
2019-06-04 21:57:18 -0400

    Initial Commit

---
+++ CatFact-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CatFact-SwiftUI
+//
+//  Created by joe cherry on 6/4/19.
+//  Copyright © 2019 joe cherry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ CatFact-SwiftUITests/CatFact_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  CatFact_SwiftUITests.swift
+//  CatFact-SwiftUITests
+//
+//  Created by joe cherry on 6/4/19.
+//  Copyright © 2019 joe cherry. All rights reserved.
+//
+
+import XCTest
+@testable import CatFact_SwiftUI
+
+class CatFact_SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
